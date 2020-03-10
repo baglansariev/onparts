cbd6ac1e1746f6a355cd805a7c8de40ddb46179e
baglansariev <baglansariev@mail.ru>
2020-03-10 17:45:55 +0600

    test changes

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -13,6 +13,13 @@ class ControllerCommonHome extends Controller {
 			$this->document->addLink($canonical, 'canonical');
 		}
 
+		$slider_settings = [
+            'banner_id' => 7,
+            'width' => 1140,
+            'height' => 800
+        ];
+
+		$data['main_slider'] = $this->load->controller('extension/module/slideshow', $slider_settings);
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
 		$data['content_top'] = $this->load->controller('common/content_top');
